b94124909a15d11a0c54a3dbd12009dcd2147578
prukerim <prukerim@gmail.com>
2026-04-06 16:27:40 +0300

    groupped the related tasks together

--- tests/wikipedia.spec.js
+++ tests/wikipedia.spec.js
@@ -1,20 +1,23 @@
 const { test, expect } = require('@playwright/test');
 
-test("Wikipedia homepage has correct title", async({ page }) => {
-    await page.goto('https://www.wikipedia.org');
-    await expect(page).toHaveTitle(/Wikipedia/);
-});
+test.describe("Wikipedia search", () => {
 
-test("Wikipedia search works", async({ page }) => {
-    await page.goto('https://www.wikipedia.org');
-    await page.locator("#searchInput").fill("Playwright");
-    await page.keyboard.press("Enter");
-    await expect(page).toHaveURL(/search|Playwright/);
-});
+    test("Wikipedia homepage has correct title", async({ page }) => {
+        await page.goto('https://www.wikipedia.org');
+        await expect(page).toHaveTitle(/Wikipedia/);
+    });
 
-test("Wikipedia search using semantic locator", async({ page }) => {
-    await page.goto('https://www.wikipedia.org');
-    await page.getByLabel("Search Wikipedia").fill("Automation testing");
-    await page.getByRole("button", {name: "Search"}).click();
-    await expect(page).not.toHaveURL("https://www.wikipedia.org");
+    test("Wikipedia search works", async({ page }) => {
+        await page.goto('https://www.wikipedia.org');
+        await page.locator("#searchInput").fill("Playwright");
+        await page.keyboard.press("Enter");
+        await expect(page).toHaveURL(/search|Playwright/);
+    });
+
+    test("Wikipedia search using semantic locator", async({ page }) => {
+        await page.goto('https://www.wikipedia.org');
+        await page.getByLabel("Search Wikipedia").fill("Automation testing");
+        await page.getByRole("button", {name: "Search"}).click();
+        await expect(page).not.toHaveURL("https://www.wikipedia.org");
+    });
 });
