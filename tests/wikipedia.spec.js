7b0d6b6b22c6e1efd14abc83aae3ac19a9d7b41a
prukerim <prukerim@gmail.com>
2026-04-06 17:13:07 +0300

    refactor: move goto into beforeEach

--- tests/wikipedia.spec.js
+++ tests/wikipedia.spec.js
@@ -2,20 +2,21 @@ const { test, expect } = require('@playwright/test');
 
 test.describe("Wikipedia search", () => {
 
+    test.beforeEach(async({ page }) => {
+    await page.goto('https://www.wikipedia.org');
+    });
+
     test("Wikipedia homepage has correct title", async({ page }) => {
-        await page.goto('https://www.wikipedia.org');
         await expect(page).toHaveTitle(/Wikipedia/);
     });
 
     test("Wikipedia search works", async({ page }) => {
-        await page.goto('https://www.wikipedia.org');
         await page.locator("#searchInput").fill("Playwright");
         await page.keyboard.press("Enter");
         await expect(page).toHaveURL(/search|Playwright/);
     });
 
     test("Wikipedia search using semantic locator", async({ page }) => {
-        await page.goto('https://www.wikipedia.org');
         await page.getByLabel("Search Wikipedia").fill("Automation testing");
         await page.getByRole("button", {name: "Search"}).click();
         await expect(page).not.toHaveURL("https://www.wikipedia.org");
